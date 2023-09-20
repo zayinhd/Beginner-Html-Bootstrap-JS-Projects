1549acf6a6af85116faa607ea35e6ca10a7f3521
zayin <zayinm300@gmail.com>
2023-09-20 03:59:37 +0000

    Updated Form Validation

--- Form-Validation/app.js
+++ Form-Validation/app.js
@@ -4,55 +4,74 @@ const email = document.getElementById("email-input");
 const password = document.getElementById("password-input");
 const confirmPassword = document.getElementById("confirmPassword-input");
 const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector(".form");
 
-submitBtn.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     // Validate form inputs
     validateForm();
-
-    // Reset values to defaults
-    setDefaults();
 });
 
 // ***Functions***
-function setDefaults() {
-    username.value = "";
-    email.value = "";
-    password.value = "";
-    confirmPassword.value = "";
-}
 
 function validateForm() {
     // Check for validity of Username
     if (username.value.trim() === "") {
-        console.log("no username");
+        error(username, "Please enter your username");
     } else {
-        console.log(username.value);
+        success(username);
     }
 
     // Check for validity of Email
     if (email.value.trim() === "") {
-        console.log("no email");
+        error(email, "Please enter your Email");
+    } else if (!validEmail(email.value.trim())) {
+        error(email, "Please enter a valid Email");
     } else {
-        console.log(email.value);
+        success(email);
     }
 
     // Check for validity of password
     if (password.value === "") {
-        console.log("no password");
+        error(password, "Please enter a Password");
     } else if (password.value.length < 8) {
-        console.log("Password too short, Must be 8 and above");
+        error(password, "Password must be atleast 8");
     } else {
-        console.log(password.value);
+        success(password);
     }
 
     // Check for validity of Confirm password
     if (confirmPassword.value === "") {
-        console.log("no password");
+        error(confirmPassword, "Please reenter your password");
     } else if (confirmPassword.value !== password.value) {
-        console.log("Password does not match");
+        error(confirmPassword, "Password does not match");
     } else {
-        console.log(confirmPassword.value);
+        success(confirmPassword);
     }
+}
+function error(element, message) {
+    const inputControl = element.parentElement;
+
+    const errorMessage = inputControl.querySelector(".input-alert");
+
+    element.style.borderColor = "red";
+    errorMessage.innerText = message;
+    errorMessage.classList.add("text-danger");
+    errorMessage.classList.remove("text-success");
+}
+
+function success(element) {
+    const inputControl = element.parentElement;
+
+    const successControl = inputControl.querySelector(".input-alert");
+
+    element.style.borderColor = "green";
+    successControl.innerText = "";
+}
+
+function validEmail(email) {
+    const valid =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return valid.test(String(email).toLowerCase());
 }
--- Form-Validation/newFile.js
+++
@@ -1,1 +0,0 @@
-submitBtn.addEventListener("click", function () {});
